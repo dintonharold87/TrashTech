a5e37bcd13c021e526f55a8aec07c380224359c8
Dinton Harold Ainemukama <dintonharold@gmail.com>
2023-06-07 13:23:15 +0300

    CRUD operations for drivers

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,4 +1,6 @@
 const { Admin } = require("../models/adminModel");
+const { Driver } = require("../models/registerDriverModel");
+const { GarbageTruck } = require("../models/truckModel");
 const bcrypt = require("bcrypt");
 
 // Register admin
@@ -25,5 +27,62 @@ exports.createAdmin = async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(400).json({ status: "fail", message: error.message });
+  }
+};
+// Create a new driver
+exports.createDriver = async (req, res) => {
+  try {
+    const { name, contact, age, licenseNumber } = req.body;
+    const driver = new Driver({ name, contact, age, licenseNumber });
+    await driver.save();
+    res.status(201).json({ message: "Driver created successfully", driver });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Failed to create driver" });
+  }
+};
+
+// Update a driver
+exports.updateDriver = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, contact, age, licenseNumber } = req.body;
+    const updatedDriver = await Driver.findByIdAndUpdate(
+      id,
+      { name, contact, age, licenseNumber },
+      { new: true }
+    );
+    if (!updatedDriver) {
+      return res.status(404).json({ message: "Driver not found" });
+    }
+    res.json({ message: "Driver updated successfully", driver: updatedDriver });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Failed to update driver" });
+  }
+};
+
+// Delete a driver
+exports.deleteDriver = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedDriver = await Driver.findByIdAndRemove(id);
+    if (!deletedDriver) {
+      return res.status(404).json({ message: 'Driver not found' });
+    }
+    res.json({ message: 'Driver deleted successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Failed to delete driver' });
+  }
+};
+
+// Get all drivers
+exports.getAllDrivers = async (req, res) => {
+  try {
+    const drivers = await Driver.find();
+    res.json(drivers);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 };
