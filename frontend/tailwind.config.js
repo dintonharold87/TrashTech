4edbaa7be1d2a062c7b25c675345d4ee55da4bc4
Dinton Harold Ainemukama <dintonharold@gmail.com>
2023-06-11 18:25:53 +0300

    Added necessary files for frontend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      montserrat: ['Montserrat', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        // Configure your color palette here
+        
+      },
+    },
+  },
+  plugins: [],
+};
