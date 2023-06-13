33a1e7f7cba48af858957a91e4677fccc72931c4
Dinton Harold Ainemukama <dintonharold@gmail.com>
2023-06-13 16:53:53 +0300

    Added the FAQs section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import HeroSection from "../components/HeroSection";
 import About from "../components/About";
+import Faqs from "../components/Faqs"
 const Home = () => {
   return (
     <>
       <HeroSection />
       <About />
+      <Faqs />
     </>
   );
 };
