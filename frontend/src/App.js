0ee064459aa840fa1a9e935d1d65ef5547b3f0da
Dinton Harold Ainemukama <dintonharold@gmail.com>
2023-06-13 18:51:03 +0300

    Added the contact section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,18 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Home from "./pages/Home";
 import { Routes, Route} from "react-router-dom";
+import About from "./components/About";
+import Faqs from "./components/Faqs";
+import Contact from "./components/Contact";
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/faqs" element={<Faqs />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </>
   );
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import HeroSection from "../components/HeroSection";
 import About from "../components/About";
 import Faqs from "../components/Faqs"
+import Contact from "../components/Contact";
 const Home = () => {
   return (
     <>
       <HeroSection />
       <About />
       <Faqs />
+      <Contact/>
     </>
   );
 };
