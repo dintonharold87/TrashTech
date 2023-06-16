50d21995f09356ae3a69ed6401f658d1bfd31ebf
Dinton Harold Ainemukama <dintonharold@gmail.com>
2023-06-16 13:12:31 +0300

    Truck Registration Form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import AdminRegistration from "./components/AdminRegistration";
 import ClientRegistration from "./components/ClientRegistration";
 import AdminDashboard from "./components/AdminDashboard";
 import DriverRegistration from "./components/RegDriver";
+import TruckRegistration from "./components/RegTruck";
 import DriverList from "./components/DriverList";
 import TruckList from "./components/TruckList";
 
@@ -17,9 +18,12 @@ function App() {
   const location = useLocation();
   const isDashboardRoute = location.pathname.startsWith("/admin_dashboard");
   const isRegisterDriver = location.pathname.startsWith("/reg_driver");
+  const isRegisterTruck = location.pathname.startsWith("/reg_truck");
   return (
     <>
-      {isDashboardRoute || isRegisterDriver ? null : <Navbar />}
+      {isDashboardRoute || isRegisterDriver || isRegisterTruck ? null : (
+        <Navbar />
+      )}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -29,6 +33,7 @@ function App() {
         <Route path="/admin_registration" element={<AdminRegistration />} />
         <Route path="/client_registration" element={<ClientRegistration />} />
         <Route path="/reg_driver" element={<DriverRegistration />} />
+        <Route path="/reg_truck" element={<TruckRegistration />} />
         <Route path="/admin_dashboard" element={<AdminDashboard />} />
         <Route path="/drivers" element={<DriverList />} />
         <Route path="/trucks" element={<TruckList />} />
